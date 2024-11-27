86ab3e7426b2f281e844dae7f52a584243239a44
Kaloyan <i500862@fhict.nl>
2024-11-27 14:56:39 +0100

    added error boundary on the application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,36 +7,42 @@ import ShopForm from "./components/ShopForm";
 import InventoryForm from "./components/InventoryForm";
 import SaleForm from "./components/SaleForm";
 import Dashboard from "./components/Dashboard";
+import ErrorBoundary from "./config/ErrorBoundary";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="container mx-auto mt-4">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
+    <ErrorBoundary>
+      <Router>
+        <div className="container mx-auto mt-4">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
 
-          {/* Customer Routes */}
-          <Route path="/add" element={<CustomerForm />} />
-          <Route path="/edit/:id" element={<CustomerForm />} />
-          <Route path="/customers/:customerId/sales" element={<CustomerSales />} />
+            {/* Customer Routes */}
+            <Route path="/add" element={<CustomerForm />} />
+            <Route path="/edit/:id" element={<CustomerForm />} />
+            <Route
+              path="/customers/:customerId/sales"
+              element={<CustomerSales />}
+            />
 
-          {/* Product Routes */}
-          <Route path="/products/add" element={<ProductForm />} />
-          <Route path="/products/edit/:id" element={<ProductForm />} />
+            {/* Product Routes */}
+            <Route path="/products/add" element={<ProductForm />} />
+            <Route path="/products/edit/:id" element={<ProductForm />} />
 
-          {/* Shop Routes */}
-          <Route path="/shops/add" element={<ShopForm />} />
-          <Route path="/shops/edit/:id" element={<ShopForm />} />
+            {/* Shop Routes */}
+            <Route path="/shops/add" element={<ShopForm />} />
+            <Route path="/shops/edit/:id" element={<ShopForm />} />
 
-          {/* Inventory Routes */}
-          <Route path="/inventory/add" element={<InventoryForm />} />
-          <Route path="/inventory/edit/:id" element={<InventoryForm />} />
+            {/* Inventory Routes */}
+            <Route path="/inventory/add" element={<InventoryForm />} />
+            <Route path="/inventory/edit/:id" element={<InventoryForm />} />
 
-          {/* Sales Routes */}
-          <Route path="/sales/add" element={<SaleForm />} />        
-        </Routes>
-      </div>
-    </Router>
+            {/* Sales Routes */}
+            <Route path="/sales/add" element={<SaleForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </ErrorBoundary>
   );
 };
 
